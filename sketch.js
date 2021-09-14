d14d069eaf59dbd59354a216f1f7ca42717b8933
JacobinaByjus <jacobinalarissa@gmail.com>
2021-09-14 14:29:37 -0300

    vs2

--- sketch.js
+++ sketch.js
@@ -1,4 +1,4 @@
-var trex, trex_running, trex_collided;
+var trex, trexRunning, trexCollided;
 var ground, invisibleGround, groundImage;
 
 var cloud, cloudsGroup, cloudImage;
@@ -8,40 +8,40 @@ var score;
 
 
 function preload(){
-  trex_running = loadAnimation("trex1.png","trex3.png","trex4.png");
-  trex_collided = loadAnimation("trex_collided.png");
-  
+  trexRunning = loadAnimation("trex1.png","trex3.png","trex4.png");
+  trexCollided = loadAnimation("trexCollided.png");
+
   groundImage = loadImage("ground2.png");
-  
+
   cloudImage = loadImage("cloud.png");
-  
+
   obstacle1 = loadImage("obstacle1.png");
   obstacle2 = loadImage("obstacle2.png");
   obstacle3 = loadImage("obstacle3.png");
   obstacle4 = loadImage("obstacle4.png");
   obstacle5 = loadImage("obstacle5.png");
   obstacle6 = loadImage("obstacle6.png");
-  
+
 }
 
 function setup() {
   createCanvas(600, 200);
-  
+
   trex = createSprite(50,180,20,50);
-  trex.addAnimation("running", trex_running);
-  trex.addAnimation("collided" , trex_collided)
+  trex.addAnimation("running", trexRunning);
+  trex.addAnimation("collided" , trexCollided)
   trex.scale = 0.5;
-  
+
   ground = createSprite(200,180,400,20);
   ground.addImage("ground",groundImage);
   ground.x = ground.width /2;
   ground.velocityX = -4;
-  
+
   invisibleGround = createSprite(200,190,400,10);
   invisibleGround.visible = false;
-  
+
   console.log("Olá" + 5);
-  
+
   score = 0;
 }
 
@@ -49,26 +49,26 @@ function draw() {
   background(180);
   text("Pontuação: "+ score, 500,50);
   score = score + Math.round(frameCount/60);
-  
-  
+
+
   if(keyDown("space")&& trex.y >= 100) {
     trex.velocityY = -13;
   }
-  
+
   trex.velocityY = trex.velocityY + 0.8
-  
+
   if (ground.x < 0){
     ground.x = ground.width/2;
   }
-  
+
   trex.collide(invisibleGround);
-  
+
   //gere as nuvens
   spawnClouds();
-  
+
   //gere obstáculos no solo
   spawnObstacles();
-  
+
   drawSprites();
 }
 
@@ -77,7 +77,7 @@ function spawnObstacles(){
    var obstacle = createSprite(400,165,10,40);
    obstacle.velocityX = -6;
 
-   
+
     // //gerar obstáculos aleatórios
     var rand = Math.round(random(1,6));
     switch(rand) {
@@ -95,8 +95,8 @@ function spawnObstacles(){
               break;
       default: break;
     }
-   
-    //atribuir escala e vida útil ao obstáculo             
+
+    //atribuir escala e vida útil ao obstáculo
     obstacle.scale = 0.5;
     obstacle.lifetime = 300;
  }
@@ -113,13 +113,13 @@ function spawnClouds() {
     cloud.addImage(cloudImage);
     cloud.scale = 0.5;
     cloud.velocityX = -3;
-    
+
      //atribuir vida útil à variável
     cloud.lifetime = 200;
-    
+
     //ajustar a profundidade
     cloud.depth = trex.depth;
     trex.depth = trex.depth + 1;
   }
-  
+
 }
